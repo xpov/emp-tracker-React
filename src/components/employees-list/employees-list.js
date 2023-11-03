9df098768edf9324eff1493bb8d77b54795dac93
Ilia F <Ilia>
2023-11-03 12:33:02 -0400

    props

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,12 +2,19 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+
+    const elements = data.map(item => {
+        const {id, ...itemProps} = item;
+
+        return (
+            <EmployeesListItem key={id} {...itemProps}/>  // name={item.name} salary={item.salary}
+        )
+    });
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            {elements}
         </ul>
     )
 }
